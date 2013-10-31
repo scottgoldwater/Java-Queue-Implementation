0903f558779097de0f1cc104749f82258fe48615
Scott Goldwater <scottgoldwater@gmail.com>
2013-10-31 17:35:21 -0400

    fixed some bugs with memory + added better input checking + fixed look and feel

--- test/queue/QueueTest.java
+++ test/queue/QueueTest.java
@@ -4,10 +4,6 @@
  */
 package queue;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
